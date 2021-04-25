b58d432d66f24e31a1a031ac3f1b4bd8e0d08923
Balázs Márton <bmarton87@gmail.com>
2021-04-25 21:20:03 +0200

    feature: create and save letter

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+INPUT_FOLDER = 'Input'
+
+def create_letter_content_for(name: str):
+    with open(f'{INPUT_FOLDER}/Letters/starting_letter.txt', 'r') as template_letter:
+        letter_content = template_letter.read()
+    return letter_content.replace('[name]', name)
+
+def save_letter_file(name: str, letter_content: str):
+    with open(f'Output/ReadyToSend/LetterTo{name}.txt', 'w') as letter_file:
+        letter_file.write(letter_content)
+
+def create_letter_for(name: str):
+    letter_content = create_letter_content_for(name)
+    save_letter_file(name, letter_content)
+
+with open(f'{INPUT_FOLDER}/Names/invited_names.txt', 'r') as invited_names:
+    names = invited_names.readlines()
+for name in names:
+    cleaned_name = name.strip()
+    create_letter_for(cleaned_name)
